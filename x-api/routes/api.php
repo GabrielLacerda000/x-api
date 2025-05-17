b8e134a8c63ae8f74af6b39e5bff3c8f142ccc57
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2025-05-17 13:10:47 -0300

    feat; rota de tweets e criar tweet

--- x-api/routes/api.php
+++ x-api/routes/api.php
@@ -1,8 +1,35 @@
 <?php
 
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('tweet', function () {
+        request()->validate([
+            'content' => 'required|max:140|string',
+        ]);
+
+        $tweet = request()->user()->tweets()->create([
+            'content' => request('content'),
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Tweet created successfully',
+            'data' => $tweet
+        ], 201);
+    });
+
+    Route::get('tweets', function () {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Tweets retrieved successfully',
+            'data' => Tweet::all()
+        ], 200);
+    });
 });
