1a61dcecf305da8b429dab5ad431688309b09de3
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2025-05-17 13:21:52 -0300

    feat: factory de tweets

---
+++ x-api/database/factories/TweetFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
+ */
+class TweetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'content' => fake()->text(140),
+            'user_id' => rand(1, 10),
+            'created_at' => fake()->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => fake()->dateTimeBetween('-1 year', 'now'),
+            'image' => null
+        ];
+    }
+}
